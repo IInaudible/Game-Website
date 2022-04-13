a59db86b9869a536a9192b141c15ab04ce3355e2
michaelberish <mikeyberish@gmail.com>
2022-04-13 15:53:11 -0700

    login

--- js/global.js
+++ js/global.js
@@ -14,11 +14,59 @@ $(document).ready(function() {
                 <li><a href="newsfeed.html">News Feed</a><div></div></li>
                 <li><a href="leaderboard.html">Leaderboard</a><div></div></li>
                 <li><a href="resources.html" class="active">Resources</a><div></div></li>
+                <button id="signin">Login</button>
             </ul>
         </div>
     `;
     $('nav').append(navBar);
 
+    let modal1 = `
+        <div id="modal-In">
+            <div id="modal-content">
+                <div id="modal-texts">
+                    <br>
+                    <label>Username:</label>
+                    <br>
+                    <input type="user" placeholder="Username" required>
+                    <br>
+                    <br>
+                    <label>Password:</label>
+                    <br>
+                    <input type="password" placeholder="Password" required>
+                </div>
+                <div id="modal-buttons">
+                    <p>Don't have an account?</p>
+                    <button id="signup-modal">Signup</button>
+                    <button id="login">Login</button>
+                </div>
+            </div>
+        </div>
+    `;
+    let modal2 = `
+        <div id="modal-Up">
+            <div id="modal-content">
+                <div id="modal-texts">
+                    <br>
+                    <label>Username:</label>
+                    <br>
+                    <input type="user" placeholder="Username" required>
+                    <br>
+                    <br>
+                    <label>Password:</label>
+                    <br>
+                    <input type="password" placeholder="Password" required>
+                </div>
+                <div id="modal-buttons">
+                    <p>Already have an account?</p>
+                    <button id="login-modal">Login</button>
+                    <button id="signup">Signup</button>
+                </div>
+            </div>
+        </div>
+    `;
+    $('main').append(modal1);
+    $('main').append(modal2);
+    
     let footer = `
         <div id="copyright">©Copyright 2022 Otter Run. All Rights Reserved.</div>
         <div id="logo-footer">
@@ -35,6 +83,35 @@ $(document).ready(function() {
             </ul>
         </div>
     `;
-    $('footer').append(footer)
+    $('footer').append(footer);
+
+    $(`#signin`).on('click', function() {
+        $('#modal-In').css('display', 'flex')
+    });
+
+    $(`#signup-modal`).on('click', function() {
+        $('#modal-In').css('display', 'none')
+        $('#modal-Up').css('display', 'flex')
+    });
+
+    $(`#login-modal`).on('click', function() {
+        $('#modal-Up').css('display', 'none')
+        $('#modal-In').css('display', 'flex')
+    });
+
+    $('#modal-In').on('click', function(e) {
+        const { target } = e;
+        if (target.id === 'modal-In') {
+            $('#modal-In').css('display', 'none');
+        }
+    });
+
+    $('#modal-Up').on('click', function(e) {
+        const { target } = e;
+        if (target.id === 'modal-Up') {
+            $('#modal-Up').css('display', 'none');
+        }
+    });
 });
+
 
